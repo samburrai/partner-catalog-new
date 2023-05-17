5bd231e65c765edc0e3736b84703910e38ac31c5
Burr Sutter <burrsutter@gmail.com>
2023-05-17 13:37:36 -0400

    SER82 feature

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -41,8 +41,8 @@ public class DemoApplication {
       simpleDateFormat = new SimpleDateFormat(pattern);
       String stringNow = simpleDateFormat.format(new Date());
 
-      System.out.println("Namaste3: " + cnt);
-      return "Namaste3 Spring " + cnt++ + " " + stringNow + " on " + HOSTNAME;
+      System.out.println("Aloha: " + cnt);
+      return "Aloha Spring " + cnt++ + " " + stringNow + " on " + HOSTNAME;
     }
 
     public static void main(String[] args) {
